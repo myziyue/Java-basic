7e88706460fc6ea87e074a20c96876671254f41c
evan2884 <evan2884@gmail.com>
2017-08-16 00:42:08 +0800

    chapter01

---
+++ src/com/myziyue/ch01/ses02/TypeDemo.java
@@ -0,0 +1,13 @@
+package com.myziyue.ch01.ses02;
+
+public class TypeDemo {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+
+        System.out.println(0x12);
+        System.out.println(12334235L);
+        System.out.println(0b0111);
+        System.out.println(1_000_000);
+    }
+}
