6045d935800d218b3e1b8416bc38db20d736a2e5
evan2884 <evan2884@gmail.com>
2017-08-17 11:46:40 +0800

    chapter01

--- src/com/myziyue/ch01/ses04/FloorModTest.java
+++ src/com/myziyue/ch01/ses04/FloorModTest.java
@@ -1,7 +1,16 @@
 package com.myziyue.ch01.ses04;
 
+import java.math.BigInteger;
+
 public class FloorModTest {
     public static void main(String[] args) {
         System.out.println(Math.floorMod(13, 12));
+
+        BigInteger n = BigInteger.valueOf(876543210123456789L);
+        System.out.println(n);
+        BigInteger k = new BigInteger("9876543210123456789");
+        System.out.println(k);
+        BigInteger r = BigInteger.valueOf(5).multiply(n.add(k)); // r = 5 * (n + k)
+        System.out.println(r);
     }
 }
