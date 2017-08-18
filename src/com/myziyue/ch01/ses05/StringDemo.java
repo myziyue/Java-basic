47ff85ab83e9f59f9173391b75d9895c98faa1d0
evan2884 <evan2884@gmail.com>
2017-08-18 14:28:56 +0800

    chapter01

---
+++ src/com/myziyue/ch01/ses05/StringDemo.java
@@ -0,0 +1,27 @@
+package com.myziyue.ch01.ses05;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String names = String.join(",", "Peter", "Paul", "Mary");
+        System.out.println(names);
+        String test = names + " are goods freids";
+        System.out.println(test);
+
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        while(i < 10000){
+            builder.append("test" + i + "\n\r");
+            i++;
+        }
+        System.out.println(builder.toString());
+
+        String nameStr = "Peter, Paul, Mary";
+        String[] result = nameStr.split(", ");
+        System.out.println(nameStr.substring(7, 10));
+        i = 0;
+        while(i < result.length) {
+            System.out.println(result[i]);
+            i++;
+        }
+    }
+}
