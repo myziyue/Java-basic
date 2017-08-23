832d21f0646fc150ec788da5d51272ae64c84744
evan2884 <evan2884@gmail.com>
2017-08-23 18:15:34 +0800

    chapter01

---
+++ src/com/myziyue/ch01/ses07/ArrayTest.java
@@ -0,0 +1,12 @@
+package com.myziyue.ch01.ses07;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arrInt = new int[10];
+        String[] arrStr = new String[10];
+        for (int i = 0; i < arrInt.length; i++) {
+            System.out.println("int is " + arrInt[i]);
+            System.out.println("string is " + arrStr[i]);
+        }
+    }
+}
